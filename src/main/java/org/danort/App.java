153aab359c5861af98cbf6853bc6d965287c81dd
Dan Ortega <ortega.dan2010@gmail.com>
2021-02-01 09:15:04 -0500

    Getting all results and fixing junit

--- src/main/java/org/danort/App.java
+++ src/main/java/org/danort/App.java
@@ -17,40 +17,46 @@ public class App {
         // THE IMAGE GIVEN WOULD BE A SUBIMAGE EXTRACTED FROM THE CALCULATED COORDINATES
         // CORRESPONDING TO THE AREA TO ANALIZE ... AND THE AREA WILL BE BROUGHT FROM
         // THE CORRESPONDENCE TO THE COORDINATES GOT FROM KEYS FROM TEXT
-        // TO GET THAT SUB AREA DO... 
-        // originalBufferedImage.getSubimage(x, y, width, height) 
+        // TO GET THAT SUB AREA DO...
+        // originalBufferedImage.getSubimage(x, y, width, height)
 
         // File imageFile = new
         // File("/home/danort/Desktop/WORKING_DAVIVIENDA/javaOmr/image.png");
-        File imageFile = new File("src/main/java/resources/dark0.png");
 
-        BufferedImage bi = ImageIO.read(imageFile);
+        for (int i = 0; i < 5; i++) {
+            System.out.println();
+            System.out.println("Image Level " + i);
 
-        int width = bi.getWidth();
-        int height = bi.getHeight();
+            File imageFile = new File("src/main/java/resources/dark" + i + ".png");
 
-        System.out.println(width + "x" + height);
+            BufferedImage bi = ImageIO.read(imageFile);
 
-        int howMany = width * height;
-        int sum = 0;
+            int width = bi.getWidth();
+            int height = bi.getHeight();
 
-        for (int c = 0; c < width; c++) {
-            for (int r = 0; r < height; r++) {
-                // ANDing the resulting color integer with 255 (0xff) in order to get the result
-                // for the first channel (blue) ... since this is for grayscaled images all
-                // non-alpha channels should have the same level of light, otherwise there would
-                // be color seen
-                sum += (bi.getRGB(c, r) & 0xff);
+            System.out.println(width + "x" + height);
+
+            int howMany = width * height;
+            int sum = 0;
+
+            for (int c = 0; c < width; c++) {
+                for (int r = 0; r < height; r++) {
+                    // ANDing the resulting color integer with 255 (0xff) in order to get the result
+                    // for the first channel (blue) ... since this is for grayscaled images all
+                    // non-alpha channels should have the same level of light, otherwise there would
+                    // be color seen
+                    sum += (bi.getRGB(c, r) & 0xff);
+                }
             }
+
+            System.out.println("Sum = " + sum);
+            int avg = sum / howMany;
+            System.out.println("Avg = " + sum / howMany);
+            int lightOrWhitePercentage = (avg * 100) / 255;
+            System.out.println("Light or white % = " + lightOrWhitePercentage);
+            System.out.println("Pixelation % = " + (lightOrWhitePercentage - 100) * -1);
+
         }
 
-        System.out.println("Sum = " + sum);
-        int avg = sum / howMany;
-        System.out.println("Avg = " + sum / howMany);
-        int lightOrWhitePercentage = (avg * 100) / 255;
-        System.out.println("Light or white % = " + lightOrWhitePercentage);
-        System.out.println("Pixelation % = " + (lightOrWhitePercentage - 100) * -1);
-
     }
-
 }
